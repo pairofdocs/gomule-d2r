c6cb3836587d88ff79d744108239e0a798115851
pairofdocs <seercthree@gmail.com>
2021-05-27 00:56:18 -0700

    fix old blank lines. 2

--- gomule/src/gomule/gui/D2FileManager.java
+++ gomule/src/gomule/gui/D2FileManager.java
@@ -1498,7 +1498,7 @@ public class D2FileManager extends JFrame
 			{
 				throw new Exception("Character is not Hardcore (HC), this is a project requirement");
 			}
-			
+
 			System.err.println("Add Char: " + pFileName );
 			iItemLists.put(pFileName, lList);
 			iViewProject.notifyItemListRead(pFileName);
